db1c116ce02bc0947cfef8463b70a09a3ab22181
ummu2222 <umangsomtiya8083@gmail.com>
2021-02-10 19:24:31 +0530

    updatee

--- server.js
+++ server.js
@@ -5,8 +5,22 @@
 
 const express = require('express');
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+  
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+  
+    next();
+  });
+
 const app = express(); // calling express fun to eccess objects
 const path = require('path');
+
+
+
 const cors = require('cors');
 
 const PORT = process.env.PORT || 3000 ;
